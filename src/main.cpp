9584f2df7ea520a6647cde38775719742252a3c7
AJ Alves <aj.zerokol@gmail.com>
2012-08-28 20:55:23 -0300

    pausa

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * HidroeletricaReservatorio.h
+ *
+ *  Created on: Aug 28, 2012
+ *      Author: zerokol
+ */
+
+// Importando bibliotecas necessárias
+#include <iostream>
+#include <vector> // Para fácil uso de vetores
+#include <string> // Para trabalhar fácil com strings
+#include <sstream> // Para trabalhar fácil com strings
+#include <math.h> // Para ajudar em calculos matemáticos
+#include <ctime>  // Para usar o time() e fornecer um bom seed para o random
+#include <cstdlib> // Para gerar numeros randômicos
+
+// Constantes
+#define INVALID -1
+
+using namespace std;
+
+// Variáveis
+
+int main(int argc, char *argv[]) {
+	//cin.get(); // aguarda por um novo caracter para então encerrar a aplicação
+
+	return 0;
+}
