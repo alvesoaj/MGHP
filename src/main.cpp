867f2118d7bcbe6a23d91099082b70667f608b2a
AJ Alves <aj.alves@zerokol.com>
2013-03-20 22:57:04 -0300

    30

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@
 using namespace std;
 
 // Constantes POSH
-#define INTERVALOS 61
+#define INTERVALOS 31
 #define DEMANDA 3472 // 3472 MW
 #define QUANTIDADE_USINAS 2
 // Constantes ACO
@@ -34,8 +34,8 @@ using namespace std;
 #define EVAPORATION_RATE 0.25
 #define POSITIVE_CONTS 0.1
 // Constantes BCO
-#define BCO_POPULATION_SIZE 200
-#define MAX_NUM_CYCLES 100000
+#define BCO_POPULATION_SIZE 100
+#define MAX_NUM_CYCLES 5000
 
 // Variáveis
 Conversor conversor;
